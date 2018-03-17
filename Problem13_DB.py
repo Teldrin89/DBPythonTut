8c02f4c84b81830f1946d1ca276649add235b721
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-03-17 16:11:03 +0100

    Problem13 solution - DB - including some explanation and addition

--- Problem13_DB.py
+++ Problem13_DB.py
@@ -8,13 +8,21 @@ while True:
     # Yes or No question
     # Get input and make it work for y or n
     createEntry = input("Enter customer (Yes/No): ")
+    # Gets 1st char from string and makes it lower case
     createEntry = createEntry[0].lower()
-    # Check to leave loop
+    # Check to leave loop - remember that to check the variable
+    # content you need logical operator (hence "==")
     if createEntry == "n":
         break
     # Get customer data
-    else:
+    elif createEntry == "y":
         fName, lName = input("Enter customer name: ").split()
+    # Added the scenario in which user puts neither "yes" nor "no"
+    elif createEntry != "n" and createEntry != "y":
+        print("You've put something wrong")
+        break
+
+
     # Add customer data to list
     customer.append({"fName": fName, "lName": lName})
 

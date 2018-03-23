c5c3b04ecfb243ee4ae3af6fe59928b0e2de3c39
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-03-23 23:28:24 +0100

    LtP9 - getters and setters - small editorial and commentary changes

--- LtP9_square.py
+++ LtP9_square.py
@@ -4,17 +4,23 @@
 
 
 class Square:
-
+    # Initialize class with self, height and width
     def __init__(self, height="0", width="0"):
-        # assign the variables height and width
+        # assign the variables: height and width
         self.height = height
         self.width = width
 
-    # create a getter
+    # create a getter - @property to refer to individual variables
+    # to get these values
     @property
     def height(self):
+        # printout the resulting value - height
         print("Retrieving the Height")
+        # __ - this way the value is protected
         return self.__height
+
+    # define setter (height as setter) to protect height from
+    # bad value setting - in this case it should be a number
 
     @height.setter
     def height(self, value):
@@ -23,10 +29,13 @@ class Square:
         else:
             print("Please only enter numbers for height")
 
+    # similar getter as in height case
+
     @property
     def width(self):
         print("Retrieving the Width")
         return self.__width
+    # similar setter as in height case
 
     @width.setter
     def width(self, value):
@@ -35,23 +44,30 @@ class Square:
         else:
             print("Please only enter numbers for width")
 
+    # one more useful function to calculate the area of square
+
     def getArea(self):
         return int(self.__width)*int(self.__height)
 
 
+# The main method - for running the function
 def main():
+    # assigning it to aSquare variable
     aSquare = Square()
-
+    # for both "height" and "width" it is possible to call them
+    # by their exact names (like without "()") because of the
+    # property and setter option
     height = input("Enter Height: ")
     width = input("Enter Width: ")
 
     aSquare.height = height
     aSquare.width = width
-
+    # Call out the height and width of square
     print("Height: ",aSquare.height)
     print("Width: ", aSquare.width)
-
+    # Call out the area of square
     print("Area is: ", aSquare.getArea())
 
 
+# Call main function to execute
 main()

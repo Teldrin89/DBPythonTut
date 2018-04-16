58b4c570a725dc2122a41f071e13ae44fc958d80
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-04-16 19:23:41 +0200

    Problem17 - initial commit, only instructions

---
+++ Problem17.py
@@ -0,0 +1,8 @@
+# Problem 17:
+# - create a file named mydata2.txt
+# - use methods from LtP8 how to open a file without "with"
+#   (open in "try")
+# - catch FileNotFoundError
+# - in "else" print contents
+# - in finally do something
+# - open nonexistent file mydata3.txt

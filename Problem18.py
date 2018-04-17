a684e9c7130967dc56421e3252634ff75fd94a5f
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-04-17 22:50:55 +0200

    Problem18 - initial commit, only problem establish comments

---
+++ Problem18.py
@@ -0,0 +1,6 @@
+# Problem 18:
+# - create a function, that receives a list and a function
+# - the function passed will return True or False if a list
+#   value is odd
+# - the surrounding function will return a list of odd numbers
+#

45afcb311df430d59e79425f95ffd6c2df344612
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-05-13 19:06:40 +0200

    LtP13 - initial commit with general description of iterable and iterator + examples of iterable code with custom classes and dictionaries

--- LtP13_iters.py
+++ LtP13_iters.py
@@ -11,3 +11,47 @@ print("Char: ", next(strSample))
 # using the same command 2nd time is returning the next
 # character from sample string
 print("Char: ", next(strSample))
+
+
+# create a class with iterator behavior
+# create an alphabet class
+class Alphabet:
+    # initialize class with all letters and an index
+    # that iteration will start with
+    def __init__(self):
+        self.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        self.index = -1
+
+    # create iteration method of this class
+    def __iter__(self):
+        # return this object
+        return self
+
+    # define next method in this class
+    def __next__(self):
+        # create if statement to sto iteration only if
+        # there is no more elements in string
+        if self.index >= len(self.letters) - 1:
+            # stop iteration exception
+            raise StopIteration
+        # increment value of index
+        self.index += 1
+        # return each character from string
+        return self.letters[self.index]
+
+
+# create a custom object and assign it to the class
+alpha = Alphabet()
+# loop for all letters in alpha using custom iteration
+# from created Alphabet class
+for letters in alpha:
+    print(letters)
+
+# through dictionary (which is iterable) it is possible
+# to iterate without a method "keys"
+# create a dictionary with 2 entries
+name = {"fName": "Luke", "lName": "Cies"}
+# run for loop with key as iterator through dictionary
+for key in name:
+    # printout the results
+    print(key, name[key])

192d752f8fa1d593993336b6f70c1109d13a40aa
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-05-19 22:59:02 +0200

    LtP14 - subclass threads - defined class and function, finished execution and added additional info on sleep time

--- LtP14_subclass_threads.py
+++ LtP14_subclass_threads.py
@@ -32,4 +32,27 @@ def get_time(name):
     rand_sleep_time = random.randint(1, 5)
     # pause thread for number of seconds from random
     time.sleep(rand_sleep_time)
+    # added one more printout msg with number of
+    # seconds given thread sleeps
+    print("Thread {} sleeps for {} seconds".format(
+        name,
+        rand_sleep_time))
 
+
+# create thread objects
+thread1 = CustomThread("1")
+thread2 = CustomThread("2")
+# start execution of both threads
+thread1.start()
+thread2.start()
+# check if thread is still "alive" (running)
+print("Thread 1 Allive: ", thread1.is_alive())
+print("Thread 2 Allive: ", thread2.is_alive())
+# printout the name of threads
+print("Thread 1 Name: ", thread1.getName())
+print("Thread 2 Name: ", thread2.getName())
+# use join to wait for both treads to end before exit
+thread1.join()
+thread2.join()
+# printout msg after script has ended work
+print("Program ended")

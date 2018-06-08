b7b31b8cbdad316e447f1ce73544305ed2249470
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-06-08 22:43:35 +0200

    LtP15 - regular expressions

---
+++ LtP15_regular_expressions.py
@@ -0,0 +1,5 @@
+# Regular expressions (Regex) are used to:
+# 1. Search for a specific string in a large amount of data
+# 2. Verify that a string has the proper format (e.g. email, phone)
+# 3. Find a string and replace it with another string
+# 4. Format data into the proper form for importing for example

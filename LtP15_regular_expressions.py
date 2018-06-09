9645b40dfff936011a49be10d0d34d73da97af02
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-06-09 23:29:54 +0200

    LtP15 - finding strings

--- LtP15_regular_expressions.py
+++ LtP15_regular_expressions.py
@@ -3,3 +3,26 @@
 # 2. Verify that a string has the proper format (e.g. email, phone)
 # 3. Find a string and replace it with another string
 # 4. Format data into the proper form for importing for example
+
+# import regular expression module
+import re
+# search for word "ape" in another string
+if re.search("ape", "The ape was at the apex"):
+    # search returns true or false, put a msg in case it's true
+    print("There is an ape")
+
+# re.findall - a method to find all the matches
+allApes = re.findall("ape", "The ape was at the apex")
+# printout all "ape"s found
+for i in allApes:
+    print(i)
+# it printed out the "ape" 2 times but only the "ape" part for
+# the word "apex" as it searches only for exact string
+
+# to return full "apex" we can use "." after word that will
+# be used to search for a string of "ape" plus 1 character or
+# or space
+allApes2 = re.findall("ape.", "The ape was at the apex")
+# printout all "ape"s found
+for i in allApes2:
+    print(i)

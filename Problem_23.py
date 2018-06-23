ae4534234c5c0ca8ece1f8dca680f4fcd9420637
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-06-23 23:43:55 +0200

    Problem 23 - email address filter, problem definition

---
+++ Problem_23.py
@@ -0,0 +1,13 @@
+import re
+
+# Problem 23:
+# Use regular expressions to match email addresses
+# from the list with set rules for what is an email
+# address:
+# 1. 1 to 20 lowercase and uppercase letters, numbers,
+#    plus ._%+- symbols
+# 2. An @ symbol
+# 3. 2 to 20 lower case and uppercase letters, numbers
+#    plus .- symbols
+# 4. A period
+# 5. 2 to 3 lowercase and uppercase letters

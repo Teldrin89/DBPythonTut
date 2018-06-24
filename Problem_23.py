1eea1ed0b8e15b44f6c00476b38e7684dff621c4
Teldrin89 <lukasz.cieslak69@gmail.com>
2018-06-24 18:13:33 +0200

    Problem 23 - email address filter, solution plus additional function writing down the found email addresses

--- Problem_23.py
+++ Problem_23.py
@@ -3,7 +3,7 @@ import re
 # Problem 23:
 # Use regular expressions to match email addresses
 # from the list with set rules for what is an email
-# address:
+# address (just find how many there are):
 # 1. 1 to 20 lowercase and uppercase letters, numbers,
 #    plus ._%+- symbols
 # 2. An @ symbol
@@ -11,3 +11,27 @@ import re
 #    plus .- symbols
 # 4. A period
 # 5. 2 to 3 lowercase and uppercase letters
+
+# create a dummy list of emails, not all following the rules
+emailList = "db@aol.com me@.com @apple.com db@.com"
+# email = "db@.com"
+# printout how many of email addresses there are that follow
+# the rules using re.findall - specified characters in []
+# brackets and number of characters in {} brackets
+print("Email Matches: ", len(re.findall("[\w._%+-]{2,20}@"
+                                        "[\w.-]{2,20}."
+                                        "[A-Za-z]{2,3}",
+                                        emailList)))
+
+# some additional solution - mine - printout the email address
+# that fulfills the criteria
+# first, split string with all addresses and create a list
+listOfEmails = emailList.split()
+# run for loop by all email addresses from the list
+for email in listOfEmails:
+    # use re.search function with the same set of criteria
+    if re.search("[\w._%+-]{2,20}@"
+                 "[\w.-]{2,20}."
+                 "[A-Za-z]{2,3}", email):
+        # printout email address if criteria are met
+        print("Email: ", email)
